a4359a71c60ab93e86eac340449c06813d9073fe
lovesimly <lovesimly@126.com>
2016-11-23 13:30:02 +0800

    update

--- src/main/java/cn/com/love/controller/TestController.java
+++ src/main/java/cn/com/love/controller/TestController.java
@@ -2,16 +2,8 @@ package cn.com.love.controller;
 
 import javax.annotation.Resource;
 
-
-
-
-
-
-
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
