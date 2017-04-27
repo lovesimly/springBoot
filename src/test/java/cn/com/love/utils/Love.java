c15280691be9c26b313599afb7f41dfbbfb3b9f3
lujingzhong <lovesimly@126.com>
2017-04-27 22:31:41 +0800

    commit

---
+++ src/test/java/cn/com/love/utils/Love.java
@@ -0,0 +1,14 @@
+package cn.com.love.utils;
+
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2017/4/26.
+ */
+public class Love {
+
+    @Test
+    public  void test(){
+        System.out.println("i love you");
+    }
+}
