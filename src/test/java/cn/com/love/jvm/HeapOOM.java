f3307b21ad87961266c0df488afdf2d104f6e090
lujingzhong <lovesimly@126.com>
2018-02-11 21:50:29 +0800

    commmit

---
+++ src/test/java/cn/com/love/jvm/HeapOOM.java
@@ -0,0 +1,17 @@
+package cn.com.love.jvm;
+
+/**
+ * Created by Administrator on 2017/9/14.
+ */
+public class HeapOOM {
+    static class  OOMObject{
+
+    }
+
+    public static void main(String[] args){
+        System.out.println("s");
+
+
+    }
+
+}
