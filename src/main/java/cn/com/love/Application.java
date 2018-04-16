d51f6a22095a34fcb560401185eced06688549d3
lovesimly <lovesimly@126.com>
2018-04-16 15:58:31 +0800

    xxxx

--- src/main/java/cn/com/love/Application.java
+++ src/main/java/cn/com/love/Application.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.ImportResource;
 @ImportResource("classpath:biz/*.xml")
 public class Application {
     //branche testxxxddd
-    //ss x f  ssfxx
+    //fuck
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
     }
